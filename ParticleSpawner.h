1ce1e7d3d9a251dc37885107921e92d60d64926c
hafiz703 <mhbt-703@hotmail.com>
2017-04-02 20:36:08 +0800

    First Commit

---
+++ ParticleSpawner.h
@@ -0,0 +1,28 @@
+#ifndef PARTICLESPAWNER_H
+#define PARTICLESPAWNER_H
+
+#include <vecmath.h>
+#include <vector>
+#ifdef _WIN32
+#include "GL/freeglut.h"
+#else
+#include <GL/glut.h>
+#endif
+
+#include "particleSystem.h"
+
+class ParticleSpawner : public ParticleSystem
+{
+public:
+	ParticleSpawner(int numParticles);
+
+	vector<Vector3f> evalF(vector<Vector3f> state);
+
+	void draw();
+	vector<Vector2f> particles;
+	int ParticleSpawner::random(int upp, int low = 0);
+
+
+};
+
+#endif
